93283f3bf387179abb3ac82f4035f7fbdc6e851b
msri0004@student.monash.edu <msri0004@student.monash.edu>
2020-11-03 15:05:53 +1100

    Get the previous dates

--- app.py
+++ app.py
@@ -2,10 +2,20 @@ from flask import Flask,request
 from flask_cors import CORS, cross_origin
 from flask import jsonify
 from find_tax_amount import apply_tax
+import firebase_admin
+from firebase_admin import credentials
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+
 
 #VARIABLES
 TOTAL_TAX_PER_WEEK = 700
 
+#Open up the firebase 
+cred = credentials.Certificate("Secrets/secret_key.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -31,6 +41,22 @@ def find_tax_amount():
     
     #Return the data with now the tax there 
     return jsonify(data)
+
+
+@app.route('/HistoryData/PreviousDates', methods=["GET"])
+@cross_origin()
+def get_historic_date_data():
+
+    #Get the for the documents
+    field_id = request.args["id"]
+
+    #Get the document from the database
+    doc = db.collection(u'HistoryData').document("PreviousDates").get()
+
+    #Return this data as json
+    return jsonify(doc.get(field_id))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
